cf16eb2855ffdac3021b1495f934ca1e9c0ef8be
Jamie Lord <jamie@lord.technology>
2021-06-25 22:32:25 +0100

    Generate Front Matter and convert basic text to markdown

--- NotionToJekyll/Program.cs
+++ NotionToJekyll/Program.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Notion.Client;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace NotionToJekyll
 {
@@ -32,15 +35,147 @@ namespace NotionToJekyll
 
             foreach (Page post in posts.Results)
             {
-                var blocks = await blocksClient.RetrieveChildrenAsync(post.Id);
+                var postFrontMatter = new PostFrontMatter
+                {
+                    Title = ((TitlePropertyValue)post.Properties["Title"]).Title.First().PlainText,
+                    Date = DateTime.Parse(((LastEditedTimePropertyValue)post.Properties["Updated"]).LastEditedTime),
+                    NotionId = post.Id
+                };
+
+                var serializer = new SerializerBuilder()
+                    .WithNamingConvention(PascalCaseNamingConvention.Instance)
+                    .Build();
+                var yaml = serializer.Serialize(postFrontMatter);
 
                 // Construct frontmatter
                 var postFile = "---\n";
-                postFile += $"title: {((TitlePropertyValue)post.Properties["Title"]).Title.First().PlainText}\n";
-                postFile += $"notion-id: {post.Id}\n";
-                postFile += $"date: {((LastEditedTimePropertyValue)post.Properties["Updated"]).LastEditedTime}\n";
+                postFile += yaml;
                 postFile += "---\n\n";
+
+                PaginatedList<Block> blocks = await blocksClient.RetrieveChildrenAsync(post.Id);
+                foreach (Block block in blocks.Results)
+                {
+                    switch (block.Type)
+                    {
+                        case BlockType.Paragraph:
+                            postFile += ((ParagraphBlock)block).Paragraph.Text.RichTextToMarkdown();
+                            break;
+                        case BlockType.Heading_1:
+                            postFile += $"# {((HeadingOneBlock)block).Heading_1.Text.RichTextToMarkdown()}";
+                            break;
+                        case BlockType.Heading_2:
+                            postFile += $"## {((HeadingTwoBlock)block).Heading_2.Text.RichTextToMarkdown()}";
+                            break;
+                        case BlockType.Heading_3:
+                            postFile += $"### {((HeadingThreeeBlock)block).Heading_3.Text.RichTextToMarkdown()}";
+                            break;
+                        case BlockType.BulletedListItem:
+                            break;
+                        case BlockType.NumberedListItem:
+                            break;
+                        case BlockType.ToDo:
+                            break;
+                        case BlockType.Toggle:
+                            break;
+                        case BlockType.ChildPage:
+                            break;
+                        case BlockType.Unsupported:
+                            break;
+                        default:
+                            break;
+                    }
+                    postFile += "\n\n";
+                }
             }
+        }
+
+        public class PostFrontMatter
+        {
+            [YamlMember(Alias = "title")]
+            public string Title { get; set; }
+
+            [YamlMember(Alias = "date")]
+            public DateTime Date { get; set; }
+
+            [YamlMember(Alias = "notion_id")]
+            public string NotionId { get; set; }
+
+            [YamlMember(Alias = "categories")]
+            public string[] Categories { get; set; }
+
+            [YamlMember(Alias = "tags")]
+            public string[] Tags { get; set; }
+        }
+    }
+
+    public static class MarkdownHelper
+    {
+        public static string RichTextToMarkdown(this IEnumerable<RichTextBase> text)
+        {
+            string output = "";
+
+            foreach (RichTextBase t in text)
+            {
+                switch (t.Type)
+                {
+                    case RichTextType.Text:
+                        output += ((RichTextText)t).RichTextToMarkdown();
+                        break;
+                    case RichTextType.Unknown:
+                    case RichTextType.Mention:
+                    case RichTextType.Equation:
+                        throw new Exception($"Unsupported RichTextBase of type {t.Type}");
+                }
+            }
+
+            return output;
+        }
+
+        private static string RichTextToMarkdown(this IEnumerable<RichTextText> text)
+        {
+            string output = "";
+
+            foreach (RichTextText richText in text)
+            {
+                output += richText.RichTextToMarkdown();
+            }
+
+            return output;
+        }
+
+        private static string RichTextToMarkdown(this RichTextText richText)
+        {
+            if (richText.Annotations.IsBold && richText.Annotations.IsItalic)
+            {
+                return $"***{richText.PlainText}***";
+            }
+
+            if (richText.Annotations.IsBold)
+            {
+                return $"**{richText.PlainText}**";
+            }
+
+            if (richText.Annotations.IsItalic)
+            {
+                return $"*{richText.PlainText}*";
+            }
+
+            if (richText.Annotations.IsCode)
+            {
+                return $"`{richText.PlainText}`";
+            }
+
+            if (richText.Annotations.IsStrikeThrough)
+            {
+                return $"~~{richText.PlainText}~~";
+            }
+
+            if (richText.Annotations.IsUnderline)
+            {
+                return $"<u>{richText.PlainText}<u/>";
+            }
+
+            return richText.PlainText;
         }
     }
 }
